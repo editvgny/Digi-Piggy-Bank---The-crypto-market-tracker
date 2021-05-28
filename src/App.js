5b497892c81807999b5522c4cfd812b8407bc825
editvgny <edit.vgny@gmail.com>
2021-05-28 14:25:20 +0200

    Created router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+          <Route exact path="/" component={Home}/>
+      </Router>
   );
 }
 
