ab571a48ab92586fede38353f729df4c0ce5ce54
editvgny <edit.vgny@gmail.com>
2021-06-01 16:04:28 +0200

    Added routes for portfolio and watchlist in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Watchlist from './components/Watchlist';
 import Header from "./components/elements/Header";
 import TopMenu from "./components/elements/TopMenu";
 
@@ -12,6 +14,8 @@ function App() {
             <TopMenu />
             <div className="App">
                 <Route exact path="/" component={Home}/>
+                <Route path="/portfolio" component={Portfolio}/>
+                <Route path="/watchlist" component={Watchlist}/>
             </div>
         </Router>
     );
