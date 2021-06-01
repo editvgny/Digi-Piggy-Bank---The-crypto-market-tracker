0dc8c4b2bf69120672bbbe31dc9084b33445b213
Bitó Attila <bitoati1987@gmail.com>
2021-06-01 11:44:46 +0200

    .config created

--- src/components/tools/MarketData.js
+++ src/components/tools/MarketData.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import configData from "../../.config.json";
+
 
 export const MarketData = (dependencies) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
     useEffect(() => {
-        axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=986c3e46-36a2-44d4-82cf-b2d00720eac1`)
+        axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${configData.Api.API_KEY}`)
             .then((response) => {
                 console.log(response.data.data)
                 setData(response.data.data)
