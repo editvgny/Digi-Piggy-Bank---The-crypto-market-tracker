effd0ea56f15ffc0be6278c201756c0a1e6c1889
editvgny <edit.vgny@gmail.com>
2021-06-07 14:06:20 +0200

    Formatted

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {MarketData} from "./tools/MarketData";
 import {
+    Link,
     makeStyles, Paper,
     Table, TableBody, TableCell,
     TableContainer, TableHead, TableRow,
 } from "@material-ui/core";
 
 export default function Home() {
-
     const [isLoading, data] = MarketData([]);
 
     const useStyles = makeStyles({
@@ -21,6 +21,10 @@ export default function Home() {
             table: {
                 minWidth: 650,
             },
+            profile_link: {
+                textDecoration: "none",
+                color: "black",
+            }
         }
     )
 
@@ -57,12 +61,15 @@ export default function Home() {
                                         <TableCell> <img
                                             src={"https://s2.coinmarketcap.com/static/img/coins/64x64/" + row.id + ".png"}
                                             alt="icon"/></TableCell>
-                                        <TableCell align="left">{row.name}</TableCell>
+                                        <TableCell align="left">
+                                            <Link className={"profile_link"}
+                                              href={"coins/" + row.id}>{row.name} {" - "} {row.symbol}</Link>
+                                        </TableCell>
                                         <TableCell>${row.quote.USD.price}</TableCell>
                                         <TableCell>${row.quote.USD.market_cap}</TableCell>
-                                        <TableCell>{row.quote.USD.percent_change_1h}</TableCell>
-                                        <TableCell>{row.quote.USD.percent_change_24h}</TableCell>
-                                        <TableCell>{row.quote.USD.percent_change_7d}</TableCell>
+                                        <TableCell>{row.quote.USD.percent_change_1h}%</TableCell>
+                                        <TableCell>{row.quote.USD.percent_change_24h}%</TableCell>
+                                        <TableCell>{row.quote.USD.percent_change_7d}%</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
