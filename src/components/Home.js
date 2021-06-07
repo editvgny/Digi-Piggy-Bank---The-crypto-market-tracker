a0f0eb16b0b5bdcbf263da55be7c27537ccbb331
editvgny <edit.vgny@gmail.com>
2021-06-07 11:49:59 +0200

    Table component added to display data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
-import Translator from "./tools/Translator";
+import React from 'react';
 import {MarketData} from "./tools/MarketData";
-import {makeStyles} from "@material-ui/core";
-
+import {
+    makeStyles, Paper,
+    Table, TableBody, TableCell,
+    TableContainer, TableHead, TableRow,
+} from "@material-ui/core";
 
 export default function Home() {
 
@@ -10,32 +12,54 @@ export default function Home() {
 
     const useStyles = makeStyles({
             content: {
-                top: "90px",
-                position: "relative",
-            }
+                top: "150px",
+                position: "absolute",
+                left: "10%",
+                right: "10%"
+
+            },
+            table: {
+                minWidth: 650,
+            },
         }
     )
 
     const classes = useStyles();
 
-    if(isLoading){
+    if (isLoading) {
         return (
             <React.Fragment>
                 Loading
             </React.Fragment>
         )
-    }else{
+    } else {
         return (
             <React.Fragment>
                 <div className={classes.content}>
-                    <Translator text="home"/>
-                    <div>{data.map(item => (
-                        <div key={item.id}> {item.name} </div>
-                    ))}
-                    </div>
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>#</TableCell>
+                                    <TableCell>Coin</TableCell>
+                                    <TableCell>Price</TableCell>
+                                    <TableCell>Market cap</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {data.map((row) => (
+                                    <TableRow key={row.id}>
+                                        <TableCell>{row.cmc_rank}</TableCell>
+                                        <TableCell align="left">{row.name}</TableCell>
+                                        <TableCell>${row.quote.USD.price}</TableCell>
+                                        <TableCell>${row.quote.USD.market_cap}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </div>
             </React.Fragment>
         )
     }
-
 }
